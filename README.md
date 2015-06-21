c567ec997cf3132a49a1592d07efc9a908107180
David Krmpotic <david.krmpotic@gmail.com>
2015-06-21 21:25:06 +0200

    fix downloads plus some refactorings

---
+++ README.md
@@ -0,0 +1,19 @@
+# it-ebooks.info downloader
+
+## Easy downloading of it-ebooks from command line
+
+Examine and get all new books: `./it-ebooks.rb`
+
+Examine and get `100` books from last download: `./it-ebooks.rb 100`
+
+**y**: download to `books` subfolder, **d**: see description, **enter**: skip, **q**: quit
+
+If you want to start at some particular book number, just edit `mark.dat`
+
+## Exclude books with specific keywords
+
+`cp exclude.txt.sample exclude.txt` and edit according to your preferences
+
+## Todo
+
+[it-ebooks](http://it-ebooks.info) actually has an api so if this HTML scraping code breaks, I will make it use the api.
--- it-ebooks.rb
+++ it-ebooks.rb
@@ -1,7 +1,10 @@
+#!/usr/bin/env ruby
 require 'nokogiri'
 require 'open-uri'
+require 'fileutils'
 
 MARK_FILE = "mark.dat"
+SITE = "http://it-ebooks.info"
 
 def wait_for_threads(threads)
   print "Waiting for downloads to finish..."
@@ -17,15 +20,23 @@ def quit(num, threads)
   exit
 end
 
-#EXCLUDE = %w(ActionScript flash flex xslt metro active_directory PostgreSQL autodesk ibm bible wpf ejb vmware j2me Java visual_studio c# f# Erlang Clojure Windows awk css html html5 php mysql jsp head_first sql oracle wordpress mercurial moodle iOS flex flash cisco drupal joomla ajax jquery javascript linq linux iphone access rails ruby perl python adobe javafx scala cocoa objective-c BlackBerry opengl netbeans dummies agile blender biztalk mac embedded jvm android mongo mongodb solr dojo asp.net sharepoint microsoft ipad asterisk voip silverlight uml vim emacs R jira redis node xaml cms sams jdk roo jruby xcode powershell solidworks mootools blend dart node.js angular.js couchdb coffeescript virtualization dreamweaver spring excel access puppet enterprise ipv6 entity cassandra sap sip office apache grails visual_basic visual_studio xml .net hadoop quickbooks lotus gradle jboss openstack xna phonegap unity game)
-
-EXCLUDE = %w(ActionScript flash flex xslt metro active_directory PostgreSQL autodesk ibm bible wpf ejb vmware j2me Java visual_studio c# f# Erlang Clojure Windows awk css php mysql jsp head_first sql oracle wordpress mercurial moodle iOS flex flash cisco drupal joomla ajax linq iphone access perl adobe javafx cocoa objective-c BlackBerry opengl netbeans dummies agile blender biztalk embedded jvm android solr dojo asp.net sharepoint microsoft ipad asterisk voip silverlight uml vim emacs R jira xaml cms sams jdk roo xcode powershell solidworks mootools blend dart couchdb virtualization dreamweaver spring excel access puppet enterprise ipv6 entity cassandra sap sip office apache grails visual_basic visual_studio xml .net hadoop quickbooks lotus gradle jboss openstack xna phonegap unity game)
-
 def exclude?(book)
-  for word in EXCLUDE
-    return true if book[:title].gsub('#', 'AAA') =~ /\b#{word.gsub('#', 'AAA').gsub('_', ' ')}\b/i
+  file = 'exclude.txt'
+  if File.exists?(file)
+    for word in File.read(file).chomp.split
+      return true if book[:title].gsub('#', 'AAA') =~ /\b#{word.gsub('#', 'AAA').gsub('_', ' ')}\b/i
+    end
   end
   false
+end
+
+def prompt(options = {})
+  puts
+  desc = options[:description] ? '/ d' : ''
+  print "y / [enter] #{desc} / q: ".squeeze
+  input = STDIN.gets.chomp.downcase
+  puts
+  input
 end
 
 if ARGV.first == 'help'
@@ -36,10 +47,8 @@ if ARGV.first == 'help'
   exit
 end
 
-puts "Working..."
-
 mark = File.exists?(MARK_FILE) ? File.read(MARK_FILE).to_i : 0
-max_num = Nokogiri::HTML(open('http://it-ebooks.info/')).css('a').select { |a| a['href'] =~ /\/book\// }.map { |a| a[:href].match(/book\/(\d+)/)[1].to_i }.max
+max_num = Nokogiri::HTML(open(SITE)).css('a').select { |a| a['href'] =~ /\/book\// }.map { |a| a[:href].match(/book\/(\d+)/)[1].to_i }.max
 
 latest = ARGV.first ? [mark + ARGV.first.to_i, max_num].min : max_num
 
@@ -47,7 +56,7 @@ books = []
 nothing_to_do = true
 for num in (mark + 1).upto(latest)
   nothing_to_do = false
-  page = open("http://it-ebooks.info/book/#{num}/")
+  page = open("#{SITE}/book/#{num}/")
   if page.base_uri.path == "/404/"
     puts "#{num} - Not Found"
   else
@@ -61,7 +70,7 @@ for num in (mark + 1).upto(latest)
     book[:publisher] = doc.css("a[itemprop='publisher']").text
     book[:description] = doc.css("span[itemprop='description']").text
     book[:year] = doc.css("b[itemprop='datePublished']").text
-    #book[:link] = "http://it-ebooks.info#{doc.css('a#dl').first['href']}"
+    book[:link] = doc.css("a").find { |link| link['href'] =~ /filepi\.com/ }['href']
 
     if exclude?(book)
       puts "Excluding #{num}: #{book[:title]} (#{book[:year]})"
@@ -80,40 +89,44 @@ books.each_with_index do |book, index|
   puts book[:title]
   puts book[:subtitle] unless book[:subtitle].empty?
   puts "#{book[:publisher]}, #{book[:year]}"
-
-  input = STDIN.gets.chomp.downcase
+  input = prompt(description: true)
 
   quit(book[:num], threads) if input == "q"
 
   if input == "d"
     puts book[:description]
-    input = STDIN.gets.chomp.downcase
+    input = prompt
     quit(book[:num], threads) if input == "q"
   end
 
   if input == "y"
     threads << Thread.new do
       begin
-        f = open(book[:link])
+        f = open(book[:link], "Referer" => SITE)
+
         cd = f.meta['content-disposition']
 
         if cd.nil? # content-disposition was missing, try again
-          f = open(book[:link])
+          f = open(book[:link], "Referer" => SITE)
           cd = f.meta['content-disposition']
         end
 
-        filename = cd.match(/filename=(\"?)(.+)\1/)[2]
+        base = 'books'
+        FileUtils.mkdir_p base
 
-        if File.exists?(filename)
-          puts "#{filename} already exists!"
+        filepath = File.join(base, cd.match(/filename=(\"?)(.+)\1/)[2])
+
+        if File.exists?(filepath)
+          puts "#{filepath} already exists!"
         else
-          File.open(filename, "wb") do |file|
+          File.open(filepath, "wb") do |file|
             file.write f.read
           end
         end
       rescue => e
         puts
-        puts "### Error downloading book #{book[:num]}: #{e.message} ###"
+        puts "### Error downloading book #{book[:num]}: #{e.message}"
+        puts e.backtrace
       end
     end
     puts
